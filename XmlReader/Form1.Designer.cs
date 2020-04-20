fa85dcc18a0b2c0a940dbf93c10c9b4a627c451e
Denis Perepelytsia <d99p01@gmail.com>
2020-04-20 20:02:44 +0300

    First commit

---
+++ XmlReader/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace XmlReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ShowSchemaButton = new System.Windows.Forms.Button();
+            this.XmlDataSet = new System.Data.DataSet();
+            this.DownloadXmlButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XmlDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ShowSchemaButton
+            // 
+            resources.ApplyResources(this.ShowSchemaButton, "ShowSchemaButton");
+            this.ShowSchemaButton.Name = "ShowSchemaButton";
+            this.ShowSchemaButton.UseVisualStyleBackColor = true;
+            this.ShowSchemaButton.Click += new System.EventHandler(this.ShowSchemaButton_Click);
+            // 
+            // XmlDataSet
+            // 
+            this.XmlDataSet.DataSetName = "XmlDataSet";
+            // 
+            // DownloadXmlButton
+            // 
+            resources.ApplyResources(this.DownloadXmlButton, "DownloadXmlButton");
+            this.DownloadXmlButton.Name = "DownloadXmlButton";
+            this.DownloadXmlButton.UseVisualStyleBackColor = true;
+            this.DownloadXmlButton.Click += new System.EventHandler(this.DownloadXmlButton_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.DownloadXmlButton);
+            this.Controls.Add(this.ShowSchemaButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XmlDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ShowSchemaButton;
+        private System.Data.DataSet XmlDataSet;
+        private System.Windows.Forms.Button DownloadXmlButton;
+    }
+}
+
---
+++ XmlReader/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Diagnostics;
+using System.IO;
+
+namespace XmlReader
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1 = new OpenFileDialog()
+            {
+                FileName = "Виберіть Xml файл",
+                Filter = "Xml files (*.xml)|*.xml",
+                Title = "Open xlm file"
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void ReadXmlButton_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\d99p\source\repos\XmlReader\XmlReader\XmlGroups.xml";
+
+            XmlDataSet.ReadXml(filePath);
+
+            dataGridView1.DataSource = XmlDataSet;
+            dataGridView1.DataMember = "Document";
+        }
+
+        private void ShowSchemaButton_Click(object sender, EventArgs e)
+        {
+            System.IO.StringWriter swXML = new System.IO.StringWriter();
+            XmlDataSet.WriteXmlSchema(swXML);
+            textBox1.Text = swXML.ToString();
+        }
+
+        private void DownloadXmlButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = openFileDialog1.FileName;
+                    using (Stream str = openFileDialog1.OpenFile())
+                    {
+                        
+                        XmlDataSet.ReadXml(filePath);
+
+                        dataGridView1.DataSource = XmlDataSet;
+                        dataGridView1.DataMember = "Document";
+                    }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+}
